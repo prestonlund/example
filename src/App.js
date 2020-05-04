4ed4088312aa2f1fb11092c1473bbf4356233179
Preston Lund <49794144+prestonlund@users.noreply.github.com>
2020-05-04 13:37:25 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>It's calculator time!</h1>
+      <h2>Time to create a calculator</h2>
+    </div>
+  );
+}
